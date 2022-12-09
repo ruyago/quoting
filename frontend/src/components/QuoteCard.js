59cd0b00537301501fe3ffdfc05c85e1e1c51e06
Ruyago <ruyago@gmail.com>
2022-12-09 16:57:09 +0100

    favourite-db

--- frontend/src/components/QuoteCard.js
+++ frontend/src/components/QuoteCard.js
@@ -3,22 +3,21 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import Favourite from "../components/Favourite";
 
 const API_URL = "http://localhost:5005";
 
 // We are deconstructing props object directly in the parentheses of the function
-function QuoteCard ( { title, description, _id, owner, refresh} ) {
+function QuoteCard ( { title, description, _id, owner, refresh, likes} ) {
   
   const {user} = useContext(AuthContext)
 
   const [quotes, setQuotes] = useState([]);
   
-  
+
   const deleteQuote = () => {
-    const storedToken = localStorage.getItem('authToken'); 
       
-    
+      const storedToken = localStorage.getItem('authToken'); 
+
     // Send the token through the request "Authorization" Headers   
     axios
       .delete(
@@ -28,6 +27,19 @@ function QuoteCard ( { title, description, _id, owner, refresh} ) {
       .then (()=> refresh())
       .catch((err) => console.log(err));
   };  
+  const addLike = () => {
+    const storedToken = localStorage.getItem('authToken'); 
+
+    // Send the token through the request "Authorization" Headers   
+    axios
+      .put(
+        `${API_URL}/api/my-quotes/add-like/${_id}`,    
+        { headers: { Authorization: `Bearer ${storedToken}` } }           
+            
+      )
+      .then (()=> refresh())
+      .catch((err) => console.log(err));
+  }; 
 
   
   return (
@@ -37,9 +49,9 @@ function QuoteCard ( { title, description, _id, owner, refresh} ) {
         </Link>
       <p style={{ maxWidth: "400px" }}>{description} </p>
       <p>@{owner} </p>
-      {owner !== user.name ? <button onClick={Favourite}>Favourite Quote</button> : <></>}
+      {owner !== user.name ? <><button onClick={addLike}>Favourite Quote</button> <p>{likes}</p></> : <></>}
      {owner === user.name ? <button onClick={deleteQuote}>Delete Quote</button> : <></>}
-     
+    
     </div>
   );
 }
